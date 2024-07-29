0b754b4baad8ca1b6e3c69df4ff78db04413e0be
Kevin2322 <vankawalakevin153@gmail.com>
2024-07-29 23:52:26 +0530

    summery

---
+++ basics/index.js
@@ -0,0 +1,29 @@
+
+// Variables
+
+const houseNo = 51;
+let houseName = "Father's Love";
+let houseCity = "surat";
+let houseState;
+//prefer not to use var 
+//beacuse of issue in block scope and functional scope
+//houseNo=52; can not assign bacause of this variable decalre by constant
+//console.table([houseNo,houseName,houseCity,houseState]);
+
+
+// Type Conversion
+let age = undefined;
+let type = Number(age);
+//console.log(type);
+
+//"33"-> 33
+//"kevin123" -> NaN
+//undefined  -> NaN
+//null -> object
+
+let isLoggedIn = 1;
+let type1 = Boolean(isLoggedIn);
+console.log(type1);
+
+//1=>true,0->false
+//""->false,"kevin"->true
