495fc2fb3403973bad5abb252c30a52243121c8c
Kevin2322 <vankawalakevin153@gmail.com>
2024-08-04 23:26:21 +0530

    contro; flow and iteration revised

---
+++ iteration/forin.js
@@ -0,0 +1,30 @@
+// for in loop only uses for objects 
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(key);// output only keys like 0,1,2,3,4
+    console.log(programming[key]); //output only values like js,rb,py,java
+}
+
+// for in loop can not works with map 
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+// can not give any ouput 
---
+++ iteration/forof.js
@@ -0,0 +1,45 @@
+
+// for of loop is only use of array iteration 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+// for of loop uses also on strings
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+
+//this code gives an error 
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
